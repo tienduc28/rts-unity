37d8da54eb031d5f653d0117f30ce10392748921
TienDuc <tienduc2803@gmail.com>
2026-07-03 16:00:10 +0700

    Add Supply Hut.prefab

--- Assets/Scripts/Units/SupplyHut.cs
+++ Assets/Scripts/Units/SupplyHut.cs
@@ -1,6 +1,34 @@
+using RTS.EventBus;
+using RTS.Events;
+using RTS.Units;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
-public class SupplyHut : MonoBehaviour
+namespace RTS.Units
 {
-    
+    public class SupplyHut : MonoBehaviour, ISelectable
+    {
+        [SerializeField] private DecalProjector decalProjector;
+        [field: SerializeField] public int Health { get; private set; }
+        public void Select()
+        {
+            if (decalProjector != null)
+            {
+                decalProjector.gameObject.SetActive(true);   
+            }
+            
+            Bus<UnitSelectedEvent>.Raise(new UnitSelectedEvent(this));
+        }
+
+        public void Deselect()
+        {
+            if (decalProjector != null)
+            {
+                decalProjector.gameObject.SetActive(false);   
+            }
+            
+            Bus<UnitDeselectedEvent>.Raise(new UnitDeselectedEvent(this));
+        }
+    }
 }
+
